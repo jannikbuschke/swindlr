f500c94831689389423ffe6bfbfcd3dbca0d6fc6
Jannik Buschke <jannik.buschke@gmail.com>
2019-09-17 00:20:25 +0200

    use own files

--- AuthenticationMiddleware.cs
+++ AuthenticationMiddleware.cs
@@ -1,27 +1,12 @@
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace SwindlR
 {
-    public static class Extensions
-    {
-        public static void UseSwindlRAuthentication(this IApplicationBuilder app)
-        {
-            app.UseMiddleware<AuthenticationMiddleware>();
-        }
-
-        public static void Swindle(this HttpClient client, string userMame, string userId)
-        {
-            client.DefaultRequestHeaders.Add(AuthenticationMiddleware.UsernameHeader, userMame);
-            client.DefaultRequestHeaders.Add(AuthenticationMiddleware.UserIdHeader, userId);
-        }
-    }
 
 
     public class AuthenticationMiddleware
---
+++ Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System.Net.Http;
+
+namespace SwindlR
+{
+    public static class Extensions
+    {
+        public static void UseSwindlRAuthentication(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<AuthenticationMiddleware>();
+        }
+
+        public static void Swindle(this HttpClient client, string userMame, string userId)
+        {
+            client.DefaultRequestHeaders.Add(AuthenticationMiddleware.UsernameHeader, userMame);
+            client.DefaultRequestHeaders.Add(AuthenticationMiddleware.UserIdHeader, userId);
+        }
+    }
+}
